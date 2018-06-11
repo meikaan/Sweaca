3c7c4173f6df683c0751d66c9e7552667b9fb5d5
Jitesh Singh <jitesh.singh@meikaan.in>
2018-06-11 16:08:34 +0530

    removed the set Logged in method

--- src/app/authguard.guard.ts
+++ src/app/authguard.guard.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot,Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { UserService } from './user.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthguardGuard implements CanActivate {
+  constructor(private user : UserService,private router : Router){}
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return true;
-  }
+    return;
+  // canActivate():boolean{
+  // 	if(!this.user.isAuthenticated()){
+  // 		this.router.navigate(['']);
+  // 		return false;
+  // 	}
+  // 		return true;
+  // }
 }
