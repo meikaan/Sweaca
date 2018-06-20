b4d7712ce6ab5101ea346fb3e1cf132d648605c2
Jitesh Singh <jitesh.singh@meikaan.in>
2018-06-20 18:33:36 +0530

    Added the navigation bar with different links

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user',
@@ -7,8 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
-
+users:Array<any>;
+  constructor(private dataService : DataService) { 
+  		// this.dataService.getUsers()
+  		// .subscribe(res => this.users = res);
+  }
   ngOnInit() {
   }
 
