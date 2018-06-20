7a1ad7999ac0ab9d6115d54d8f5eb34b68257ea8
Jitesh Singh <jitesh.singh@meikaan.in>
2018-06-20 18:30:01 +0530

    Added the contents on the homepage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,17 +11,36 @@ import { User } from '../models/user';
 })
 export class HomeComponent implements OnInit {
 home;
+buy:any;
 users : User[];
+plants:any;flowers:any;seeds:any;pots:any;
   constructor(private http:Http,private router : Router,public _user:UserService,public dataService : DataService) { }
 
   ngOnInit() {
   }
-  onClick(){
-  	this.router.navigate(['']);
+  addPlants(){
+   this.plants = parseInt(prompt("How many you want"));
+   alert("okay you are going to buy"+this.plants);
+   this.router.navigate(['checkout']);
   }
-  getUsersDetails(){
-  	this.dataService.getUsers().then(users=>
-  		this.users = users
-  		);
+  addFlowers(){
+   this.flowers = parseInt(prompt("How many you want"));
+   alert("okay you are going to buy"+this.flowers);
+   this.router.navigate(['checkout']);
   }
+  addSeeds(){
+   this.seeds = parseInt(prompt("How many you want"));
+   alert("okay you are going to buy"+this.seeds);
+   this.router.navigate(['checkout']);
+  }
+  addPots(){
+   this.pots = parseInt(prompt("How many you want"));
+   alert("okay you are going to buy"+this.pots);
+   this.router.navigate(['checkout']);  
+  } 
+  // getUsersDetails(){
+  // 	this.dataService.getUsers().then(users=>
+  // 		this.users = users
+  // 		);
+  // }
 }
