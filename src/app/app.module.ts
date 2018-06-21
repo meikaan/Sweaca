c4a44c8dc2a92c66211a68c1e102a3783f39c430
Jitesh Singh <jitesh.singh@meikaan.in>
2018-06-21 18:49:04 +0530

    new import for login,register and dashboard component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,37 +4,35 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { HttpModule } from '@angular/http' 
 import { HttpClientModule } from '@angular/common/http';
-import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { FormsModule } from '@angular/forms';
 import {APP_BASE_HREF} from '@angular/common';
 import { DataService } from './data.service';
 import { UserService } from './user.service';
-import { InMemoryDataService } from './in-memory-data.service';
 import { JwtInterceptorProvider, ErrorInterceptorProvider } from './helpers/index';
 import { AuthenticationService} from './authentication.service';
  import { AuthguardGuard } from './authguard.guard';
 // import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { loginComponent } from './user/login/login.component';
-import { RegisterComponent } from './user/register/register.component';
-import { UserComponent } from './user/user.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { CheckoutComponent } from './checkout/checkout.component';
 import { TeamComponent } from './team/team.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 //import { LoginComponent } from './user/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    loginComponent,
     RegisterComponent,
-    UserComponent,
     HomeComponent,
     ContactComponent,
     CheckoutComponent,
-    TeamComponent
+    TeamComponent,
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
@@ -42,24 +40,23 @@ import { TeamComponent } from './team/team.component';
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
-    InMemoryWebApiModule.forRoot(InMemoryDataService),
     RouterModule.forRoot([
     {
       path: '',
-      component: UserComponent 
+      component: LoginComponent 
     },
     {
     	path : 'login',
-    	component : loginComponent
+    	component : LoginComponent
     },
     {
       path : 'register',
       component : RegisterComponent
     },
     {
-      path : 'home',
+      path : 'dashboard',
       // canActivate : [AuthguardGuard],
-      component : HomeComponent
+      component : DashboardComponent
     },
     {
       path : 'contact',
@@ -72,6 +69,10 @@ import { TeamComponent } from './team/team.component';
     {
       path : 'team',
       component : TeamComponent
+    },
+    {
+      path : 'home',
+      component : HomeComponent
     }
     ]),
   ],
