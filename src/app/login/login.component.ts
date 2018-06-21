9298ee980b824a8a43d5a4030a5229bffeb07b03
Jitesh Singh <jitesh.singh@meikaan.in>
2018-06-21 18:43:27 +0530

    moved login component inside app

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,90 @@
+import { Component, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Http,HttpModule,Headers } from '@angular/http' 
+import { Router } from '@angular/router';
+import { UserService } from '../user.service';
+import { DataService } from '../data.service';
+import { AuthenticationService } from '../authentication.service';
+// import { contentHeaders } from '../../headers';
+// import { contentHeaders } from '../common/headers';
+
+@Component({
+  selector: 'app-login-component',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+loginForm;
+model:any={};
+username:string="";password:string="";
+// username:string="";
+// password:string="";
+  constructor(
+  private http:Http,
+  private router : Router,
+  public _user:UserService,
+  public dataService : DataService,
+  private authenticationService : AuthenticationService) { } 
+  ngOnInit() {
+      // this.authenticationService.logout();
+      this.loginForm = new FormGroup({
+        username : new FormControl("",Validators.required),
+        password : new FormControl("",Validators.required)
+      });
+  }
+  onSubmit(){
+  }
+  // login(){
+  //   this.authenticationService.login(this.model.username, this.model.password)
+  //           .subscribe(
+  //               data => {
+  //                   this.router.navigate(['home']);
+  //               },
+  //               error => {
+  //                 if(error){
+  //                   console.log(error);
+  //                 }
+  //                   // this.alertService.error(error);
+  //                   // this.loading = false;
+  //               });
+  // }
+}
+      // code...
+    //console.log(this.login.get('username').value);
+     // getData(){
+  //   this.http.get('https://my-json-server.typicode.com/typicode/demo/db')
+  //   .subscribe((data) => {console.log(data)})
+  //   // console.log()
+  // }
+  // let headers : Headers;
+  // let body = JSON.stringify([this.username,this.password]);
+  // headers.append("Authentication","Basic" + btoa(this.username + "this is" +this.password));
+  // headers.append("Content-Type","application/x-www-form-encoded");
+  // this.http.post('http://localhost:4200/users',body,{headers:headers})
+  // .subscribe(
+  //   response => {
+  //                 localStorage.setItem('id_token',response.json().id_token);
+  //                 this.router.navigate(['home']);
+  //               },
+  //   error =>   {
+  //                 console.log(error);
+  //              }            
+  //   );
+
+/*    onSubmit(){
+    this.username = this.login.get('username').value;
+    this.password = this.login.get('password').value;
+    if (this.username == 'jitesh' && this.password == 'jitesh') {
+    alert("Thank u for login");
+    this.login.reset();
+    // this.user.setUserLoggedIn();
+    this.router.navigate(['home']);
+    }
+    else{
+      alert("Invalid Credentials");
+      this.login.reset();
+    }*/
+
+//   }
