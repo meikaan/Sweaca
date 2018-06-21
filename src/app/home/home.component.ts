4e374e6c0ea12cfd562559d5643634b0aa7c32b0
Jitesh Singh <jitesh.singh@meikaan.in>
2018-06-21 18:45:10 +0530

    changed the user component to home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,46 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { Http,HttpModule,Headers } from '@angular/http' 
-import { UserService } from '../user.service';
 import { DataService } from '../data.service';
-import { User } from '../models/user';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-home;
-buy:any;
-users : User[];
-plants:any;flowers:any;seeds:any;pots:any;
-  constructor(private http:Http,private router : Router,public _user:UserService,public dataService : DataService) { }
 
+users:Array<any>;
+  constructor(private dataService : DataService) { 
+      // this.dataService.getUsers()
+      // .subscribe(res => this.users = res);
+  }
   ngOnInit() {
   }
-  addPlants(){
-   this.plants = parseInt(prompt("How many you want"));
-   alert("okay you are going to buy"+this.plants);
-   this.router.navigate(['checkout']);
-  }
-  addFlowers(){
-   this.flowers = parseInt(prompt("How many you want"));
-   alert("okay you are going to buy"+this.flowers);
-   this.router.navigate(['checkout']);
-  }
-  addSeeds(){
-   this.seeds = parseInt(prompt("How many you want"));
-   alert("okay you are going to buy"+this.seeds);
-   this.router.navigate(['checkout']);
-  }
-  addPots(){
-   this.pots = parseInt(prompt("How many you want"));
-   alert("okay you are going to buy"+this.pots);
-   this.router.navigate(['checkout']);  
-  } 
-  // getUsersDetails(){
-  // 	this.dataService.getUsers().then(users=>
-  // 		this.users = users
-  // 		);
-  // }
+
 }
