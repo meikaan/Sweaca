106263ccdc08b6c41efebaabd8c31de2ed14fc13
Jitesh Singh <jitesh.singh@meikaan.in>
2018-06-28 19:09:27 +0530

    used the service to get data from dashboard component

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,23 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
+import { trigger,state,style,animate,transition } from '@angular/animations';
 import { User } from '../models/user';
+
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
-  styleUrls: ['./checkout.component.css']
+  styleUrls: ['./checkout.component.css'],
+  animations : [
+  trigger('popOverState',[
+    state('show',style({
+      opacity : 1
+    })),
+    state('hide',style({
+      opacity : 0
+    })),
+    transition('show => hide',animate('600ms ease-out')),
+    transition('hide => show',animate('600ms ease-in'))
+    ])
+  ]
 })
 export class CheckoutComponent implements OnInit {
-message : string;
-hello:string;
+myTotal:any;
+show=false;
 currentUser : User;
 public value:string="";
-  constructor() { 
+  constructor(private userService:UserService) { 
     this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
   }
   ngOnInit() {
+    this.myTotal=this.userService.getFlowerData();
   }
-  // newMessage(){
-  // 	this.dataService.changeMessage("Hello from Dashboard");
-  // }
-
+  get stateName(){
+    return this.show ? 'show':'hide';
+  }
+  toggle(){
+    this.show = !this.show;
+  }
 }
