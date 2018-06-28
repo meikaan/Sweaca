9177466c38cffd8ba4761392dd5590405a2223f2
Jitesh Singh <jitesh.singh@meikaan.in>
2018-06-28 19:11:59 +0530

    used service to set the prices of the plants, flowers and seeds

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -11,31 +11,32 @@ import { trigger,state,style,animate,transition } from '@angular/animations'
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css'],
   animations: [
-    trigger('scrollAnimation', [
-      state('show', style({
-        opacity: 1,
-        transform: "translateX(0)"
-      })),
-      state('hide',   style({
-        opacity: 0,
-        transform: "translateX(-100%)"
-      })),
-      transition('show => hide', animate('700ms ease-out')),
-      transition('hide => show', animate('700ms ease-in'))
-    ])
-  ]
+          trigger('scrollAnimation', [
+          state('show', style({
+            opacity: 1,
+            transform: "translateX(0)"
+          })),
+          state('hide',   style({
+            opacity: 0,
+            transform: "translateX(100%)"
+          })),
+          transition('show => hide', animate('700ms ease-out')),
+          transition('hide => show', animate('700ms ease-in'))
+        ])]
 })
 export class DashboardComponent implements OnInit {
-home;
+message:string="Added to Cart";
+cartValue=0;
 state='hide';
 currentUser : User;
 users : User[] = [];
-message:string;
-plants:any;flowers:any;seeds:any;pots:any;
+conifer = 1999;bromeliads = 1299;lavender=2999;daisy=2499;bergamot=3299;aster=2199;flax=2099;hemp=2199;
+    seeds:any;
+    pots:any;
   constructor(
     private http:Http,
     private router : Router,
-    public _user:UserService,
+    public userService:UserService,
     public el:ElementRef
     ) { this.currentUser = JSON.parse(localStorage.getItem('currentUser')); }
 
@@ -50,32 +51,49 @@ plants:any;flowers:any;seeds:any;pots:any;
       } else {
         this.state = 'hide'
       }
-   }
-  addPlants(){
-   // this.plants = parseInt(prompt("How many you want"));
-   // alert("okay you are going to buy"+this.plants);
-   // this.dataService.myMethod(this.value);
-   this.router.navigate(['checkout']);
-
   }
-  addFlowers(){
-   this.flowers = parseInt(prompt("How many you want"));
-   alert("okay you are going to buy"+this.flowers);
-   this.router.navigate(['checkout']);
-  }
-  addSeeds(){
-   this.seeds = parseInt(prompt("How many you want"));
-   alert("okay you are going to buy"+this.seeds);
-   this.router.navigate(['checkout']);
-  }
-  addPots(){
-   this.pots = parseInt(prompt("How many you want"));
-   alert("okay you are going to buy"+this.pots);
-   this.router.navigate(['checkout']);  
-  } 
-  // getUsersDetails(){
-  // 	this.dataService.getUsers().then(users=>
-  // 		this.users = users
-  // 		);
+  // addPlants(){
+  // this.plants = parseInt(prompt("How many you want"));
+  // alert("okay you are going to buy"+this.plants);
+  // // this.dataService.myMethod(this.value);
+  // this.userService.setFlowerData(this.plants);
+  // this.router.navigate(['checkout']);
   // }
+  addConifer(){
+    this.cartValue =  this.cartValue + (this.conifer);
+    alert(this.message);
+  }
+  addBromeliads(){
+    this.cartValue =  this.cartValue + (this.bromeliads);
+    alert(this.message);
+  }
+  addFlax(){
+    this.cartValue =  this.cartValue + (this.flax);
+    alert(this.message);
+  }
+  addHemp(){
+    this.cartValue = this.cartValue + this.hemp;
+    alert(this.message);
+  }
+  addAster(){
+    this.cartValue =  this.cartValue + (this.aster);
+    alert(this.message);
+  }
+  addBergamot(){
+    this.cartValue =  this.cartValue + (this.bergamot);
+    alert(this.message);
+  }
+  addDaisy(){
+    this.cartValue =  this.cartValue + (this.daisy);
+    alert(this.message);
+  }
+  addLavender(){
+    this.cartValue =  this.cartValue + (this.lavender);
+    alert(this.message);
+  }
+  finalPayment(){
+    alert("Your total amount is Rs"+this.cartValue);
+    this.userService.setFlowerData(this.cartValue);
+    this.router.navigate(['checkout']);
+  }
 }
